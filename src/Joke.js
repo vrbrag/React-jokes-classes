e46a6ef7b70cb41d1f9bbcf5ae7013d3a3653d56
Vanessa Bragdon <vrbragdon@gmail.com>
2023-01-03 16:42:25 -0800

    joke class

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends Component {
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+  constructor(props) {
+    super(props)
+    this.upVote = this.upVote.bind(this)
+    this.downVote = this.downVote.bind(this)
+  }
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+  upVote() {
+    this.props.vote(this.props.id, +1)
+  };
+  downVote() {
+    this.props.vote(this.props.id, -1)
+  };
 
-        {votes}
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+
+          {this.props.votes}
+        </div>
+
+        <div className="Joke-text">{this.props.text}</div>
       </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+    )
+  }
 }
+
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
+
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
+
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
+
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
 
 export default Joke;
