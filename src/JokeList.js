455f420cb5e9bc9a7edd93b58eac75dffee1b958
Vanessa Bragdon <vrbragdon@gmail.com>
2023-01-03 16:10:28 -0800

    notes

--- src/JokeList.js
+++ src/JokeList.js
@@ -5,20 +5,23 @@ import "./JokeList.css";
 
 function JokeList({ numJokesToGet = 10 }) {
   const [jokes, setJokes] = useState([]);
+  // jokes = { id, joketext, vote}
 
   /* get jokes if there are no jokes */
 
-  useEffect(function() {
+  useEffect(function () {
     async function getJokes() {
       let j = [...jokes];
       let seenJokes = new Set();
+      /* object of unique values, of any type (no repeated items); uses add() to add elements that dont already exist in the obj*/
       try {
         while (j.length < numJokesToGet) {
           let res = await axios.get("https://icanhazdadjoke.com", {
             headers: { Accept: "application/json" }
           });
-          let { status, ...jokeObj } = res.data;
-  
+
+          let { status, ...jokeObj } = res.data; // deconstrutction of res.data ; jokeObj is the object of a joke {id, joke}
+
           if (!seenJokes.has(jokeObj.id)) {
             seenJokes.add(jokeObj.id);
             j.push({ ...jokeObj, votes: 0 });
@@ -42,6 +45,7 @@ function JokeList({ numJokesToGet = 10 }) {
   }
 
   /* change vote for this id by delta (+1 or -1) */
+  /* +1 or -1 from votes:0 from inside getJokes()*/
 
   function vote(id, delta) {
     setJokes(allJokes =>
@@ -52,14 +56,15 @@ function JokeList({ numJokesToGet = 10 }) {
   /* render: either loading spinner or list of sorted jokes. */
 
   if (jokes.length) {
+    /* sort in DESCENDING order */
     let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
+
     return (
       <div className="JokeList">
         <button className="JokeList-getmore" onClick={generateNewJokes}>
           Get New Jokes
         </button>
-  
+
         {sortedJokes.map(j => (
           <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
         ))}
